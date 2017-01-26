61ae32c55fb5ec372185c0a9fe8fcacc21289d01
Jon Nordby <jononor@gmail.com>
2017-01-26 18:54:25 +0100

    Add minimal Button example

---
+++ examples/Button/Button.ino
@@ -0,0 +1,88 @@
+
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266WiFi.h>
+
+#include <PubSubClient.h>
+#include <Msgflo.h>
+
+
+struct Config {
+  const String id = ""; // unused?
+
+  const int ledPin = 5;
+  const int buttonPin = 2;
+
+  const String wifiSsid = "ssid";
+  const String wifiPassword = "password";
+
+  const char *mqttHost = "mqtt.bitraf.no";
+  const int mqttPort = 1883;
+
+  const char *mqttUsername = "myuser";
+  const char *mqttPassword = "mypassword";
+} cfg;
+
+WiFiClient wifiClient;
+PubSubClient mqttClient;
+msgflo::Engine *engine;
+msgflo::OutPort *buttonPort;
+long nextButtonCheck = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  Serial.printf("Configuring wifi: %s\r\n", cfg.wifiSsid.c_str());
+  WiFi.begin(cfg.wifiSsid.c_str(), cfg.wifiPassword.c_str());
+
+  mqttClient.setServer(cfg.mqttHost, cfg.mqttPort);
+  mqttClient.setClient(wifiClient);
+
+  String clientId = "bitraf-button-";
+  clientId += WiFi.macAddress();
+
+  engine = msgflo::pubsub::createPubSubClientEngine(
+             "bitraf-iot/BlinkButton",
+             "Blinking Button",
+             "lightbulb-o",
+             &mqttClient, clientId.c_str(), cfg.mqttUsername, cfg.mqttPassword);
+
+  buttonPort = engine->addOutPort("button-event", "any", "bitraf/button/" + cfg.id + "/event");
+
+  Serial.printf("Led pin: %d\r\n", cfg.ledPin);
+  Serial.printf("Button pin: %d\r\n", cfg.buttonPin);
+
+  pinMode(cfg.buttonPin, INPUT);
+  pinMode(cfg.ledPin, OUTPUT);
+}
+
+void loop() {
+  static bool connected = false;
+
+  if (WiFi.status() == WL_CONNECTED) {
+    if (!connected) {
+      Serial.printf("Wifi connected: ip=%s\r\n", WiFi.localIP().toString().c_str());
+    }
+    connected = true;
+    engine->loop();
+  } else {
+    if (connected) {
+      connected = false;
+      Serial.println("Lost wifi connection.");
+    }
+  }
+
+  // TODO: check for statechange. If changed, send right away. Else only send every 3 seconds or so
+  if (millis() > nextButtonCheck) {
+    const bool pressed = digitalRead(cfg.buttonPin);
+    buttonPort->send(pressed ? "true" : "false");
+    nextButtonCheck += 100;
+  }
+
+//  digitalWrite(cfg.ledPin, !b);
+}
+
